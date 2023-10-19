7a4e1d2ed7fc7f29a4efa75617440297a5f0fa0f
Firelux <firelux96@gmail.com>
2023-10-19 15:08:07 +0200

    Migliorata UI

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import '../components/app_bar.dart';
-import '../components/drawer.dart';
 import '../components/bottom_navigation_bar.dart';
 import 'package:tsuyoi/modules/goal.dart';
 import 'package:tsuyoi/modules/category.dart';
+import 'package:tsuyoi/pages/category_page.dart';
+
+String? name = "Firelux";
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,8 +16,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _searchQueryController = TextEditingController();
-  static bool isSearching = false;
   String searchQuery = "SearchQuery";
   final _goalsBox = Hive.box("goals_box");
   final _categoriesBox = Hive.box("categories_box");
@@ -134,119 +133,179 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildSearchField() {
-    return TextField(
-      controller: _searchQueryController,
-      autofocus: true,
-      decoration: InputDecoration(
-        hintText: "Search Data...",
-        border: InputBorder.none,
-        hintStyle: TextStyle(color: Colors.white30),
-      ),
-      style: TextStyle(color: Colors.white, fontSize: 16.0),
-      onChanged: (query) => updateSearchQuery(query),
-    );
-  }
-
-  List<Widget> buildActions() {
-    if (isSearching) {
-      return <Widget>[
-        IconButton(
-          icon: const Icon(Icons.clear),
-          onPressed: () {
-            if (_searchQueryController == "" ||
-                _searchQueryController.text.isEmpty) {
-              Navigator.pop(context);
-              return;
-            }
-            _clearSearchQuery();
-          },
-        ),
-      ];
-    }
-
-    return <Widget>[
-      IconButton(
-        icon: const Icon(Icons.search),
-        onPressed: startSearch,
-      ),
-    ];
-  }
-
-  void startSearch() {
-    /*
-    ModalRoute.of(context)
-        .addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching));
-  */
-    setState(() {
-      isSearching = true;
-    });
-  }
-
-  void updateSearchQuery(String newQuery) {
-    setState(() {
-      searchQuery = newQuery;
-    });
-  }
-
-  void stopSearching() {
-    _clearSearchQuery();
-
-    setState(() {
-      isSearching = false;
-    });
-  }
-
-  void _clearSearchQuery() {
-    setState(() {
-      _searchQueryController.clear();
-      updateSearchQuery("");
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(),
-      drawer: drawer(context),
-      body: ListView.builder(
-        itemCount: _goals.length,
-        itemBuilder: (_, index) {
-          final currentGoal = _goals[index];
-          return Card(
-            color: Colors.blue.shade200,
-            margin: const EdgeInsets.all(10),
-            elevation: 3,
-            child: ListTile(
-              title: Text(
-                currentGoal.name,
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-              subtitle: Text(
-                currentGoal.category,
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: () => _showForm(context, currentGoal.id),
-                  ),
-                  IconButton(
-                    onPressed: () => _deleteItem(currentGoal.id),
-                    icon: const Icon(Icons.delete),
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        centerTitle: false,
+        leading: const CircleAvatar(
+          radius: 30.0,
+          backgroundImage: NetworkImage(
+              'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'),
+          backgroundColor: Colors.transparent,
+        ),
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              '$name',
+              textAlign: TextAlign.left,
             ),
-          );
-        },
+            Text(
+              "Welcome $name",
+              style: const TextStyle(fontSize: 16),
+            )
+          ],
+        ),
       ),
+
+      //drawer: drawer(context),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              height: 20,
+            ),
+          ),
+          SizedBox(
+            height: 260,
+            child: ListView.builder(
+              itemCount: _categories.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                final currentItem = _categories[index];
+                return SizedBox(
+                  width: 300,
+                  child: Card(
+                    color: Color(0xFF673AB7),
+                    margin: const EdgeInsets.all(10),
+                    elevation: 3,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 150,
+                          width: double.infinity,
+                          color: Color(0xFF2196F3),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      CategoryPage(category: currentItem),
+                                ),
+                              );
+                            },
+                            child: ListTile(
+                              title: Text(
+                                currentItem.name,
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(Icons.edit),
+                                    iconSize: 24,
+                                    onPressed: () =>
+                                        _showForm(context, currentItem.id),
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(Icons.delete),
+                                    iconSize: 24,
+                                    onPressed: () =>
+                                        _deleteItem(currentItem.id),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Spacer(),
+                        Container(
+                          child: const Text(
+                            "Progress:",
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        const Spacer(),
+                        SizedBox(
+                          width: 240,
+                          child: LinearProgressIndicator(
+                            value: 0.6,
+                            minHeight: 12,
+                            borderRadius: BorderRadius.circular(10),
+                            backgroundColor: Color(0xFF9E9E9E),
+                            valueColor: const AlwaysStoppedAnimation<Color>(
+                                Colors.green),
+                          ),
+                        ),
+                        const Spacer(),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(
+              bottom: 10,
+              top: 15,
+              left: 15,
+              right: 15,
+            ),
+            child: const Text(
+              "My Tasks",
+              style: TextStyle(fontSize: 19),
+            ),
+          ),
+          ListView.builder(
+            itemCount: _goals.length,
+            shrinkWrap: true,
+            itemBuilder: (_, index) {
+              final currentGoal = _goals[index];
+              return Card(
+                color: Colors.blue.shade200,
+                margin: const EdgeInsets.all(10),
+                elevation: 3,
+                child: ListTile(
+                  title: Text(
+                    currentGoal.name,
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  subtitle: Text(
+                    currentGoal.category,
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        iconSize: 24,
+                        onPressed: () => _showForm(context, currentGoal.id),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        iconSize: 24,
+                        onPressed: () => _deleteItem(currentGoal.id),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _showForm(context, null);
